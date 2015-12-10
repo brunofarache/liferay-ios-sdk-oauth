e93652c99d6ec4aa73aaae88b0684605f76c228f
Allan Melo <allan.melo@liferay.com>
2015-12-10 17:05:32 -0300

    Add WebviewViewController to use LROAuthWebview

---
+++ Sample/WebviewViewController.swift
@@ -0,0 +1,48 @@
+class WebviewViewController: UIViewController, LROAuthCallback {
+	
+	@IBOutlet private var _navigationItem: UINavigationItem!
+	@IBOutlet private var _webview: LROAuthWebView!
+	
+	init(config: LROAuthConfig, resultBlock:((LROAuthConfig?) -> Void)) {
+		super.init(nibName : "WebviewViewController", bundle : nil)
+
+		_config = config
+		_resultBlock = resultBlock
+	}
+		
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		_webview.allowAutomatically = true;
+		
+		_webview.start(_config, callback: self)
+		
+		let backButtonBar = UIBarButtonItem(
+			barButtonSystemItem: UIBarButtonSystemItem.Cancel,
+			target: self, action: "close")
+		
+		_navigationItem.setRightBarButtonItem(backButtonBar, animated: false)
+		
+	}
+	
+	func close() {
+		dismissViewControllerAnimated(true, completion: nil)
+	}
+	
+	func onFailure(error: NSError!) {
+		close()
+		_resultBlock(nil)
+	}
+	
+	func onSuccess(config: LROAuthConfig!) {
+		close()
+		_resultBlock(config)
+	}
+
+	private var _config:LROAuthConfig!
+	private var _resultBlock:((LROAuthConfig?) -> Void)!
+
+}
