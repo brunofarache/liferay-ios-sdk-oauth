bcaa101ae5e8d73c13822d780658250bc8306194
Allan Melo <allan.melo@liferay.com>
2015-12-10 17:12:20 -0300

    Refactor to generate new instance of LROAuthConfig with new timestamp

--- Sample/MainViewController.swift
+++ Sample/MainViewController.swift
@@ -9,24 +9,17 @@ class MainViewController: UIViewController {
 		let path = bundle?.pathForResource("settings", ofType: "plist")
 		settings = NSDictionary(contentsOfFile: path!) as! [String: String]
 
-		let server = settings["server"]
-		let consumerKey = settings["oauth_consumer_key"]
-		let consumerSecret = settings["oauth_consumer_secret"]
-		let callbackURL = settings["oauth_callback_url"]
-
-		self.config = LROAuthConfig(
-			server: server, consumerKey: consumerKey,
-			consumerSecret: consumerSecret, callbackURL: callbackURL)
-
 		super.init(nibName: "MainViewController", bundle: nil)
 	}
 
 	required init?(coder: NSCoder) {
 		super.init(coder: coder)
 	}
-
-	@IBAction func login(sender: UIButton) {
+	
+	@IBAction func loginWithBrowser(sender: UIButton) {
 		self.label.text = ""
+		
+		setupNewConfigs()
 
 		LRRequestToken.requestTokenWithConfig(
 			config,
@@ -62,32 +55,26 @@ class MainViewController: UIViewController {
 			completion: nil)
 	}
 	
+	func setupNewConfigs() {
+		let server = settings["server"]
+		let consumerKey = settings["oauth_consumer_key"]
+		let consumerSecret = settings["oauth_consumer_secret"]
+		let callbackURL = settings["oauth_callback_url"]
+		
+		self.config = LROAuthConfig(
+			server: server, consumerKey: consumerKey,
+			consumerSecret: consumerSecret, callbackURL: callbackURL)
+	}
+	
 	func accessTokenWithCallbackURL(callbackURL: NSURL) {
 		let params = LROAuth.extractRequestParams(callbackURL.query);
 		config.verifier = params["oauth_verifier"] as! String
 
 		LRAccessToken.accessTokenWithConfig(
 			config,
-			onSuccess: {
-				let oauth = LROAuth(config: $0)
-				let session = LRSession(
-					server: self.config.server, authentication: oauth)
-
-				let service = LRGroupService_v62(session: session)
-
-				do {
-					let sites = try service.getUserSites()
-					var text = ""
-
-					for site in sites {
-						text = text + (site["name"]! as! String) + "\n"
-					}
-
-					self.label.text = text
-				}
-				catch let error as NSError {
-					NSLog("%@", error)
-				}
+			onSuccess: { config in
+				self.loadData(config)
+				
 			},
 			onFailure: {
 				NSLog("%@", $0)
